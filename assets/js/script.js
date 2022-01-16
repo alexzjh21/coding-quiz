e8c450ef2218abee7bf55ce625cf72293ee10da6
alexzjh21 <alexzjh21@yahoo.co.jp>
2022-01-16 00:36:27 -0500

    add questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,66 @@ var myQuestions = [
             d: 'number'
 		},
 		correctAnswer: 'a'
+	},
+    {
+		question: "What kind of value does string hold?",
+		answers: {
+			a: 'nothing/empty',
+			b: 'true and false',
+			c: 'alphabets',
+            d: 'number'
+		},
+		correctAnswer: 'c'
+	},
+    {
+		question: "What kind of value does number hold?",
+		answers: {
+			a: 'nothing/empty',
+			b: 'true and false',
+			c: 'alphabets',
+            d: 'number'
+		},
+		correctAnswer: 'd'
+	},
+    {
+		question: "What does Date() do?",
+		answers: {
+			a: 'Creates an HTML anchor to be used as a hypertext target',
+			b: 'Parses a string representation of a date and time and then returns the internal millisecond representation for the same',
+			c: 'Returns the function that created the corresponding instance of the object',
+            d: 'Returns the present date and time'
+		},
+		correctAnswer: 'd'
+	},
+    {
+		question: "Inside which HTML element do we put the JavaScript?",
+		answers: {
+			a: '<script>',
+			b: '<js>',
+			c: '<javascript>',
+            d: '<head>'
+		},
+		correctAnswer: 'a'
+	},
+    {
+		question: "How do you create a function in JavaScript?",
+		answers: {
+			a: 'function myFunction()',
+			b: 'function : myFunction()',
+			c: 'function = myFunction()',
+            d: 'function == myFunction()'
+		},
+		correctAnswer: 'c'
+	},
+    {
+		question: "How do you write an IF statement in JavaScript?",
+		answers: {
+			a: 'if i == 5 then',
+			b: 'if i = 5',
+			c: 'if i = 5 then',
+            d: 'if (i == 5)'
+		},
+		correctAnswer: 'd'
 	}
 ];
 
